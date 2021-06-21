f1cbd3d02bf221d89e20663ba1993eb8483c5d3e
Suranjit Baroy <62200368+SuranjitBaroy@users.noreply.github.com>
2021-06-21 17:04:31 +0600

    Add files via upload
    Another Project

---
+++ learn.js
@@ -0,0 +1,128 @@
+// JS Input Out Put Project
+
+
+
+
+// var FirstName = prompt("Enter Your First Name");
+
+// var LastName  = prompt("Enter Your Last  Name");
+
+
+
+
+
+// console.log("My name is: "+ FirstName + " " +  LastName);
+
+//Math.Js (Simple Math Calculation)
+
+let num1,num2,value;
+
+num1 = 21;
+num2 = 10;
+
+value = num1 % num2;
+
+console.log("Total value is ", value);
+
+// priority or precedence *,/,+,-
+// Bracket (),{},[]
+
+var priority = (4+4)*4;
+
+console.log(priority);
+
+
+//Math Object PI
+
+let P = Math.PI;
+console.log(P);
+
+//E = Euler's 
+
+let e = Math.E;
+
+console.log(e);
+
+//R = Round
+
+let r = Math.round(7.50);
+
+console.log(r);
+
+//c = ceil
+
+let c = Math.ceil(3.1);
+
+console.log(c);
+
+//f = floor;
+
+let f = Math.floor(4.5);
+console.log(f);
+
+// a = abs(absolute)
+
+let a=Math.abs(-4);
+
+console.log(a);
+
+// p = Power 
+
+let p = Math.pow(5,2);
+
+console.log(p);
+
+
+// p = Power 
+
+let s = Math.sqrt(16);
+
+console.log(s);
+
+// min = minimum
+
+let m = Math.min(400,300,100,200);
+console.log(m);
+
+
+// max = maximum
+
+let max = Math.max(400,300,100,200);
+console.log(max);
+
+
+//random
+
+// let ran = Math.random() *10;
+// console.log(ran);
+
+let ran = Math.random() *10;
+let abc = Math.round(ran);
+console.log(ran.toFixed(4));
+// console.log(abc);
+
+// float
+let TheValue = 5.8686968;
+
+console.log(TheValue.toFixed(2));
+
+//Store Min value and max Value
+console.log(Number.MIN_VALUE);
+
+console.log(Number.MIN_VALUE);
+
+console.log(1/0);
+
+//Float to Integer
+
+let fl = 12.33;
+console.log(parseInt(fl));
+
+
+
+
+
+
+
+
+
